67a03203da680126899ec0d2c49808ea16d2bf60
victor <vgodreau@hotmail.com>
2018-02-20 16:11:57 +0100

    Amélioration des modèles

--- base_connaissances/admin.py
+++ base_connaissances/admin.py
@@ -1,21 +1,29 @@
 from django.contrib import admin
-from .models import TypeCulture
+from .models import Famille
 from .models import Espece
 from .models import Variete
-from .models import Envahisseur
-from .models import SituationGEO
-from .models import TypeProduction
-from .models import Action
+from .models import Ravageur
 from .models import Lutte
-from .models import Realistation
+from .models import PbCulture
+from .models import Action
+from .models import Pratique
+from .models import ModalitePratique
+from .models import PlantesAmies
+from .models import ZoneGEO
+from .models import PlantesAmies2
+from .models import VarieteGenerique
 
 
-admin.site.register(TypeCulture)
+admin.site.register(Famille)
 admin.site.register(Espece)
 admin.site.register(Variete)
-admin.site.register(Envahisseur)
-admin.site.register(SituationGEO)
-admin.site.register(TypeProduction)
-admin.site.register(Action)
+admin.site.register(Ravageur)
 admin.site.register(Lutte)
-admin.site.register(Realistation)
+admin.site.register(PbCulture)
+admin.site.register(Action)
+admin.site.register(Pratique)
+admin.site.register(ModalitePratique)
+admin.site.register(PlantesAmies)
+admin.site.register(ZoneGEO)
+admin.site.register(PlantesAmies2)
+admin.site.register(VarieteGenerique)
--- base_connaissances/models.py
+++ base_connaissances/models.py
@@ -1,92 +1,141 @@
 from django.db import models
-from django.utils import timezone
-from django.db.models import F
-from django.db.models.functions import Concat
 
-class TypeCulture(models.Model):
-    nom = models.CharField(max_length=200, primary_key=True)
+class Famille(models.Model):
+    id = models.AutoField(primary_key=True)
+    nom = models.CharField(max_length=200, null=False, unique=True)
 
     def __str__(self):
         return self.nom
+
 
 class Espece(models.Model):
-    type_culture = models.ForeignKey('TypeCulture')
-    nom = models.CharField(max_length=200, primary_key=True)
-    faculte_germinative = models.IntegerField(blank=True, null=True, verbose_name="faculte germinative en année")
-    temps_levee = models.IntegerField(blank=True, null=True, verbose_name="Temps de levée en jour")
-    amendement_type = models.CharField(max_length=200, blank=True, null=True)
-    amendement_qualite = models.CharField(max_length=200, blank=True, null=True)
-    amendement_quantite = models.IntegerField(blank=True, null=True)
-    remarque = models.TextField(blank=True, null=True)
-
-    def __str__(self):
-        return self.nom
-
-class Variete(models.Model):
-    espece = models.ForeignKey('Espece')
-    nom = models.CharField(max_length=200, primary_key=True)
-    remarque = models.TextField(blank=True, null=True)
-    actions = models.ManyToManyField('Action')
-    situations_geo = models.ManyToManyField('SituationGEO')
-    types_production = models.ManyToManyField('TypeProduction')
-    envahisseurs = models.ManyToManyField('Envahisseur', through='Lutte')
-    #realisation = models.ForeignKey('Realistation')
-
-    def __str__(self):
-        return self.nom
-
-class Envahisseur(models.Model):
-    nom = models.CharField(max_length=200, primary_key=True)
-    remarque = models.TextField(blank=True, null=True)
-
-    def __str__(self):
-        return self.nom
-
-class SituationGEO(models.Model):
-    nom = models.CharField(max_length=200, primary_key=True)
-    remarque = models.TextField(blank=True, null=True)
-
-    def __str__(self):
-        return self.nom
-
-class TypeProduction(models.Model):
-    nom = models.CharField(max_length=200, primary_key=True)
-    remarque = models.TextField(blank=True, null=True)
-
-    def __str__(self):
-        return self.nom
-
-class Action(models.Model):
-    nom = models.CharField(max_length=200, primary_key=True)
-    remarque = models.TextField(blank=True, null=True)
-
-    def __str__(self):
-        return self.nom
-
-class Lutte(models.Model):
     id = models.AutoField(primary_key=True)
-    variete = models.ForeignKey('Variete')
-    envahisseurs = models.ForeignKey('Envahisseur')
-    nom = models.CharField(max_length=200)
-    moyen_de_lutte = models.TextField(blank=True, null=True)
+    nom = models.CharField(max_length=200, null=False, unique=True)
+    id_famille = models.ForeignKey('Famille')
+    nom_latin_genre = models.CharField(max_length=200)
+    nom_latin_espece = models.CharField(max_length=200)
+    faculte_germinative = models.IntegerField(null=True, verbose_name="Faculté germinative en années")
+    temps_levee = models.IntegerField(null=True, verbose_name="Temps de levée en jours")
+    description = models.TextField(blank=True, null=True)
+    gen_periode_recolte = models.BooleanField(default=False)
+    gen_periode_semis = models.BooleanField(default=False)
+    gen_forme = models.BooleanField(default=False)
+    gen_conservation = models.BooleanField(default=False)
+    gen_vitesse_croissance = models.BooleanField(default=False)
+    gen_utilisation_culinaire = models.BooleanField(default=False)
+    image_espece = models.ImageField(blank=True)
 
     def __str__(self):
         return self.nom
 
-class Realistation(models.Model):
-    variete = models.ForeignKey('Variete')
-    action = models.ForeignKey('Action')
-    type_production = models.ForeignKey('TypeProduction', blank=True, null=True)
-    situation_geo = models.ForeignKey('SituationGEO', blank=True, null=True)
-    nom = models.CharField(max_length=200)
-    date_min = models.DateField()
-    date_max = models.DateField(blank=True, null=True)
-    regularite_jour = models.IntegerField(blank=True, null=True)
-    saisonnalité = models.CharField(max_length=200, blank=True, null=True)
+
+class VarieteGenerique(models.Model):
+    id = models.AutoField(primary_key=True)
+    id_espece = models.ForeignKey('Espece')
+    nom = models.CharField(max_length=200, null=False)
     remarque = models.TextField(blank=True, null=True)
+    periode_recolte = models.CharField(max_length=200,blank=True)
+    periode_semis = models.CharField(max_length=200,blank=True)
+    forme = models.CharField(max_length=200,blank=True)
+    conservation = models.CharField(max_length=200,blank=True)
+    vitesse_croissance = models.CharField(max_length=200,blank=True)
+    utilisation_culinaire = models.CharField(max_length=200,blank=True)
 
     def __str__(self):
         return self.nom
 
     class Meta:
-       unique_together = ("variete", "action", "type_production", "situation_geo")
+       unique_together = ("id_espece", "nom")
+
+class Variete(models.Model):
+    id = models.AutoField(primary_key=True)
+    id_variete_generique = models.ForeignKey('VarieteGenerique')
+    nom = models.CharField(max_length=200, null=False)
+    remarque = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.nom
+
+
+class Ravageur(models.Model):
+    id = models.AutoField(primary_key=True)
+    nom = models.CharField(max_length=200)
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.nom
+
+
+class Lutte(models.Model):
+    id = models.AutoField(primary_key=True)
+    nom = models.CharField(max_length=200)
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.nom
+
+
+class PbCulture(models.Model):
+    id = models.AutoField(primary_key=True)
+    id_ravageur = models.ForeignKey('Ravageur')
+    id_espece = models.ForeignKey('Espece')
+    id_lutte = models.ManyToManyField('Lutte')
+    description = models.TextField(blank=True, null=True)
+    image_pb_culture = models.ImageField(blank=True)
+
+
+    def __str__(self):
+        return self.nom
+
+
+class Action(models.Model):
+    id = models.AutoField(primary_key=True)
+    nom = models.CharField(max_length=200)
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.nom
+
+
+class Pratique(models.Model):
+    id = models.AutoField(primary_key=True)
+    id_espece = models.ForeignKey('Espece')
+    id_action = models.ForeignKey('Action')
+
+    def __str__(self):
+        return self.nom
+
+
+class ModalitePratique(models.Model):
+    id = models.AutoField(primary_key=True)
+    id_pratique = models.ForeignKey('Pratique')
+    n_variante = models.IntegerField(null=True)
+    id_variete_generique = models.ManyToManyField('VarieteGenerique')
+    nom_modalite = models.CharField(max_length=200)
+    valeur_modalite = models.CharField(max_length=200)
+    image_pratique = models.ImageField(blank=True)
+
+    def __str__(self):
+        return self.nom
+
+
+class PlantesAmies(models.Model):
+    id_espece1 = models.OneToOneField('Espece')
+    id_espece2 = models.ForeignKey('PlantesAmies2')
+    type_relation = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.nom
+
+
+class PlantesAmies2(models.Model):
+    id_espece2 = models.OneToOneField('Espece')
+
+    def __str__(self):
+        return self.id_espece2.nom
+
+
+class ZoneGEO(models.Model):
+    modificateur_printemps = models.IntegerField(null=True)
+    modificateur_automne = models.IntegerField(null=True)
+    modificateur_croissance = models.IntegerField(null=True)
